73121f1815867399512e4b4c7c311c94b2c14bdf
George Bills <george.s.bills@gmail.com>
2023-04-27 22:39:55 +1000

    2022 day 1

---
+++ 2022/1/Program.cs
@@ -0,0 +1,21 @@
+﻿var lines = File.ReadLines("in.txt");
+
+var elves = new List<int>() { 0 };
+foreach (string line in lines)
+{
+    if (line == "")
+    {
+        elves.Add(0); // new elf
+        continue;
+    }
+
+    int calories = int.Parse(line);
+    elves[^1] += calories;
+}
+
+int sum = elves
+    .OrderByDescending(n => n) // small enough that sorting is fine
+    .Take(3)
+    .Sum();
+
+Console.WriteLine(sum);
