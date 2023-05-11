a3dded40248aecb89f1bd6de9dda1e40f82f9a5a
George Bills <george.s.bills@gmail.com>
2023-05-11 23:45:25 +1000

    day 16 (part one working - dyn prog ftw)

--- 2022/16/Program.cs
+++ 2022/16/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Immutable;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 string file = args.Length == 1 ? args[0] : "eg.txt";
@@ -9,7 +8,7 @@ var lineRegex = new Regex(
     RegexOptions.ExplicitCapture
 );
 
-var valves = File.ReadLines(file)
+var parsed = File.ReadLines(file)
     .Select(line => lineRegex.Match(line).Groups)
     .Select(groups =>
         new
@@ -20,141 +19,74 @@ var valves = File.ReadLines(file)
         }
     );
 
-Console.WriteLine($"parsed {valves.Count()} valves");
-// Console.WriteLine(String.Join(Environment.NewLine, valves));
+int numValves = parsed.Count();
+int[] flows = parsed.Select(v => v.Flow).ToArray();
+string[] valves = parsed.Select(v => v.ID).ToArray();
+bool[,] edges = new bool[numValves, numValves];
 
-var flows = valves.ToDictionary(v => v.ID, v => v.Flow);
-var edges = valves.SelectMany(v => v.TunnelsTo.Select(t => (from: v.ID, to: t))).ToLookup(t => t.from);
-var prune = valves.Where(v => v.Flow == 0).Select(v => v.ID).ToHashSet();
-
-foreach (var e in edges)
+foreach (var valve in parsed)
 {
-    Console.WriteLine($"{e.Key}: {String.Join(',', e.Select(e => e.to).Order())}");
+    int nodeIndex = Array.FindIndex(valves, id => id == valve.ID);
+    foreach (string tunnel in valve.TunnelsTo)
+    {
+        int adjacentIndex = Array.FindIndex(valves, id => id == tunnel);
+        edges[nodeIndex, adjacentIndex] = true;
+    }
 }
 
-Console.WriteLine($"pruned: {String.Join(',', prune)}");
-
-foreach (string s in valves.Select(v => v.ID))
-{
-    var c = Connected(s, new HashSet<string>());
-    Console.WriteLine($"connected {s}: {String.Join(',', c)}");
-}
+// consider only positive flow valves that haven't been opened
+ulong consider = flows
+    .Select((flow, idx) => (flow, idx))
+    .Where(fi => fi.flow > 0)
+    .Aggregate(0ul, (acc, fi) => acc | (1ul << fi.idx));
 
 const string start = "AA";
+const int time = 30;
+int startIndex = Array.FindIndex(valves, id => id == start);
 
-var state = new State { Position = start, Pruned = prune, Flows = flows };
+var memo = new Dictionary<(int, ulong, int), int>();
+
 var sw = Stopwatch.StartNew();
-var best = Search(state);
+int best = Search(startIndex, consider);
 sw.Stop();
 
-Console.WriteLine($"maximum score is {best} (elapsed: {sw.Elapsed})");
+Console.WriteLine($"maximum score is {best} (elapsed: {sw.Elapsed}; memoised: {memo.Count()})");
 
-int Search(State state)
+int Search(int node, ulong consider, int time = time)
 {
-    if (state.Depth >= State.MaxDepth)
+    int score = 0;
+
+    if (time <= 0)
     {
-        return state.Score;
+        return score;
+    }
+
+    if (memo.TryGetValue((node, consider, time), out score))
+    {
+        return score;
     }
 
     int best = int.MinValue;
-    var moves = GenerateMoves(state);
-    foreach (var move in moves)
+
+    // search adjacent positions
+    for (int adjacent = 0; adjacent < numValves; adjacent++)
     {
-        move.MakeMove(state);
-        int score = Search(state);
-        if (score > best)
+        if (edges[node, adjacent])
         {
-            best = score;
+            score = Search(adjacent, consider, time - 1);
+            best = Math.Max(score, best);
         }
-        move.UnmakeMove(state);
     }
+
+    // possibly open the valve at our current position
+    if ((consider & (1ul << node)) != 0)
+    {
+        int released = (time - 1) * flows[node];
+        score = released + Search(node, consider ^ (1ul << node), time - 1);
+        best = Math.Max(score, best);
+    }
+
+    memo[(node, consider, time)] = best;
+
     return best;
 }
-
-IEnumerable<IMove> GenerateMoves(State state)
-{
-    yield return new MoveOpenValve(state.Position);
-
-    var connected = Connected(state.Position, new HashSet<string>());
-    var visitable = connected.Where(c => (state.Depth + c.Cost) <= State.MaxDepth);
-
-    foreach (var v in visitable)
-    {
-        yield return new MoveThroughTunnel(From: state.Position, To: v.Node, v.Cost);
-    }
-}
-
-IEnumerable<(string Node, int Cost)> Connected(string valve, ISet<string> seen, int weight = 1)
-{
-    seen.Add(valve);
-    foreach (var edge in edges[valve])
-    {
-        string to = edge.to;
-        if (seen.Contains(to))
-        {
-            break;
-        }
-        if (!prune.Contains(to))
-        {
-            yield return (to, weight); // edge directly connects valve => to
-        }
-        else
-        {
-            foreach (var transitive in Connected(to, seen, weight + 1))
-            {
-                yield return transitive; // path from valve => transitive
-            }
-        }
-    }
-}
-
-interface IMove
-{
-    void MakeMove(State state);
-    void UnmakeMove(State state);
-}
-
-record MoveThroughTunnel(string From, string To, int Cost) : IMove
-{
-    public void MakeMove(State state)
-    {
-        state.Position = To;
-        state.Depth += Cost;
-    }
-
-    public void UnmakeMove(State state)
-    {
-        state.Position = From;
-        state.Depth -= Cost;
-    }
-}
-
-record MoveOpenValve(string Valve) : IMove
-{
-    public void MakeMove(State state)
-    {
-        state.Pruned.Add(Valve);
-        state.Score += Score(state.Flows[Valve], state.Depth);
-        state.Depth += 1;
-    }
-
-    public void UnmakeMove(State state)
-    {
-        state.Pruned.Remove(Valve);
-        state.Depth -= 1;
-        state.Score -= Score(state.Flows[Valve], state.Depth);
-    }
-
-    private int Score(int flow, int depth) => flow * (State.MaxDepth - depth);
-}
-
-class State
-{
-    public const int MaxDepth = 15;
-    public string Position;
-    public int Score = 0;
-    public int Depth = 1;
-    public ISet<string> Pruned;
-
-    public IDictionary<string, int> Flows; // dirty hack to put this on the state class
-};
