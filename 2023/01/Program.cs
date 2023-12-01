885c441b841d40f0394d1ed1db2a91ec081b6a13
George Bills <george.s.bills@gmail.com>
2023-12-01 16:35:33 +1100

    2023d1p1

---
+++ 2023/01/Program.cs
@@ -0,0 +1,15 @@
+﻿string file = args.Length >= 1 ? args[0] : "eg.txt";
+
+int answer = File.ReadAllLines(file)
+    .Select(line =>
+        {
+            var digits = line.ToCharArray().Where(c => char.IsDigit(c));
+            int first = ToDigit(digits.First());
+            int last = ToDigit(digits.Last());
+            return 10 * first + last;
+        }
+    ).Sum();
+
+Console.WriteLine($"{answer}");
+
+static int ToDigit(char c) => c - '0';
