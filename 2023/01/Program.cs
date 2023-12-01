90d9261463319f7ddcf594323794ae7928f22f45
George Bills <george.s.bills@gmail.com>
2023-12-01 17:01:09 +1100

    2023d1p2

--- 2023/01/Program.cs
+++ 2023/01/Program.cs
@@ -1,15 +1,37 @@
 ﻿string file = args.Length >= 1 ? args[0] : "eg.txt";
 
+string[] digitstrs = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+char[] digitchars = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' }; /* no 0's in my input ;) */
+
 int answer = File.ReadAllLines(file)
     .Select(line =>
         {
-            var digits = line.ToCharArray().Where(c => char.IsDigit(c));
-            int first = ToDigit(digits.First());
-            int last = ToDigit(digits.Last());
-            return 10 * first + last;
+            int first = GetFirst(line);
+            int last = GetLast(line);
+            int number = 10 * first + last;
+            return number;
         }
     ).Sum();
 
-Console.WriteLine($"{answer}");
+Console.WriteLine(answer);
 
-static int ToDigit(char c) => c - '0';
+int GetFirst(string line)
+{
+    var strs = digitstrs.Select((str, idx) => (digit: idx + 1, index: line.IndexOf(str)));
+    var chs = digitchars.Select((ch, idx) => (digit: idx + 1, index: line.IndexOf(ch)));
+    return Enumerable.Concat(strs, chs)
+        .Where(digitindex => digitindex.index != -1)
+        .MinBy(digitindex => digitindex.index)
+        .digit;
+}
+
+int GetLast(string line)
+{
+    var strs = digitstrs.Select((str, idx) => (digit: idx + 1, index: line.LastIndexOf(str)));
+    var chs = digitchars.Select((ch, idx) => (digit: idx + 1, index: line.LastIndexOf(ch)));
+    return Enumerable.Concat(strs, chs)
+        .Where(digitindex => digitindex.index != -1)
+        .MaxBy(digitindex => digitindex.index)
+        .digit;
+}
