84594d65c50f13b08448eb5814e53b1a6b1c25c5
George Bills <george.s.bills@gmail.com>
2023-12-06 23:42:06 +1100

    2023d6p2

--- 2023/06/Program.cs
+++ 2023/06/Program.cs
@@ -3,22 +3,19 @@
 string file = args.Length >= 1 ? args[0] : "eg.txt";
 
 var input = File.ReadAllLines(file);
-var times = Regex.Matches(input[0], @"\d+").Select(m => m.Value).Select(int.Parse);
-var distances = Regex.Matches(input[1], @"\d+").Select(m => m.Value).Select(int.Parse);
-var races = Enumerable.Zip(times, distances);
-var solutions = races.Select(Solve).ToArray();
-var ways = solutions.Select(s => s.high - s.low + 1);
+long time = long.Parse(string.Concat(Regex.Matches(input[0], @"\d+").Select(m => m.Value)));
+long distance = long.Parse(string.Concat(Regex.Matches(input[1], @"\d+").Select(m => m.Value)));
+(long low, long high) = Solve(time, distance);
+long ways = high - low + 1;
+Console.WriteLine(ways);
 
-Console.WriteLine($"{string.Join(" x ", ways)} = {ways.Aggregate((a, b) => a * b)}");
-
-(int low, int high) Solve((int, int) race)
+(long low, long high) Solve(long time, long distance)
 {
-    (int time, int distance) = race;
     //             acceleration * (time - acceleration) = distance
     //            -acceleration^2 + time * acceleration = distance
     // -acceleration^2 + time * acceleration - distance = 0
     double discriminant = Math.Pow(time, 2) - 4 * (distance + 1);
     double solution1 = (-1 * time + Math.Sqrt(discriminant)) / -2;
     double solution2 = (-1 * time - Math.Sqrt(discriminant)) / -2;
-    return ((int)Math.Ceiling(solution1), (int)Math.Floor(solution2));
+    return ((long)Math.Ceiling(solution1), (long)Math.Floor(solution2));
 }
