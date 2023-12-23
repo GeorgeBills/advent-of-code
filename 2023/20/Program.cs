c359683705da8e658729afc832a212bfbab67668
George Bills <george.s.bills@gmail.com>
2023-12-23 23:17:39 +1100

    2023d20p1 (works for the examples but not for the input)

--- 2023/20/Program.cs
+++ 2023/20/Program.cs
@@ -1,61 +1,68 @@
 ﻿using System.Text.RegularExpressions;
 
-const string file = "eg.txt";
+const string file = "in.txt";
 
 const string broadcaster = "broadcaster";
+const string output = "output";
 
-var modules = File.ReadLines(file).Select(ParseModule).ToArray();
+var queue = new Queue<Pulse>(); // "always processed in the order they are sent"
 
-var indexes = modules.Select((m, i) => (m.Name, Index: i)).ToDictionary(ni => ni.Name, ni => ni.Index);
+var modules = File.ReadLines(file).Select(l => ParseModule(queue, l)).ToDictionary(m => m.Name, m => m);
 
-var types = modules.Select(m => m.Type).ToArray();
-
-Console.WriteLine("types: " + string.Join(',', types));
-
-int[][] outputs = modules.Select(m => m.Destinations.Select(d => indexes[d]).ToArray()).ToArray();
-
-Console.WriteLine("outputs: " + string.Join(';', outputs.Select(op => string.Join(',', op))));
-
-var inputslu = outputs.SelectMany((outidxs, modidx) => outidxs.Select(outidx => (outidx, modidx))).ToLookup(x => x.outidx, x => x.modidx);
-int[][] inputs = Enumerable.Range(0, outputs.Length).Select(i => inputslu[i].ToArray()).ToArray();
-
-Console.WriteLine("inputs: " + string.Join(';', inputs.Select(ip => string.Join(',', ip))));
-
-var pulses = new Pulse[modules.Count(), modules.Count()];
-
-var broadcasteri = indexes[broadcaster];
-
-foreach (var outputi in outputs[broadcasteri])
+// GROSS HACK: init the incoming connections for all of our conjunction modules
+//             "they initially default to remembering a low pulse for each input"
+//             otherwise if the first pulse is high then all pulses seen so far are high
+foreach (var m in modules.Values)
 {
-    pulses[broadcasteri, outputi] = Pulse.Low;
+    foreach (var d in m.Destinations)
+    {
+        if (d == output) { continue; }
+
+        if (modules.TryGetValue(d, out var n) && n is Conjunction)
+        {
+            var pulse = new Pulse(m.Name, n.Name, PulseType.Low);
+            m.HandlePulse(pulse);
+        }
+    }
+}
+queue.Clear(); // drain our "initialisation" pulses
+
+const int numPushes = 1000;
+var sent = new Dictionary<PulseType, int>();
+for (int i = 0; i < numPushes; i++)
+{
+    var initial = new Pulse(null, broadcaster, PulseType.Low);
+    queue.Enqueue(initial);
+
+    while (queue.TryDequeue(out var pulse))
+    {
+#if DEBUG
+        Console.WriteLine(pulse);
+#endif
+
+        sent[pulse.Type] = sent.GetValueOrDefault(pulse.Type, 0) + 1;
+
+        if (!modules.TryGetValue(pulse.To, out var module))
+        {
+            Console.WriteLine($"unmatched module {pulse.To}: {pulse.Type}");
+            continue;
+        }
+
+        modules[pulse.To].HandlePulse(pulse);
+    }
+#if DEBUG
+    Console.WriteLine();
+#endif
 }
 
-// "Flip-flop modules (prefix %) are either on or off; they are initially off."
-static (Pulse pulse, FlipFlopState state) FlipFlop(Pulse pulse, FlipFlopState state = FlipFlopState.Off) => pulse switch
+int low = sent[PulseType.Low];
+int high = sent[PulseType.High];
+Console.WriteLine($"saw {low} low and {high} high for an answer of {low * high}");
+
+static Module ParseModule(Queue<Pulse> queue, string modulestr)
 {
-    // "If a flip-flop module receives a high pulse, it is ignored and nothing happens."
-    Pulse.High => (Pulse.None, state),
-
-    // "If a flip-flop module receives a low pulse, it flips between on and off."
-    Pulse.Low => state switch
-    {
-        FlipFlopState.On => (Pulse.Low, FlipFlopState.Off),  // "If it was on, it turns off and sends a low pulse."
-        FlipFlopState.Off => (Pulse.High, FlipFlopState.On), // "If it was off, it turns on and sends a high pulse."
-    },
-};
-
-// "Conjunction modules (prefix &) remember the type of the most recent pulse
-//  received from each of their connected input modules; they initially default
-//  to remembering a low pulse for each input. When a pulse is received, the
-//  conjunction module first updates its memory for that input. Then, if it
-//  remembers high pulses for all inputs, it sends a low pulse; otherwise, it
-//  sends a high pulse."
-static Pulse Conjunction(params Pulse[] input) => input.All(p => p == Pulse.High) ? Pulse.Low : Pulse.High;
-
-static (string Name, ModuleType Type, IEnumerable<string> Destinations) ParseModule(string modulestr)
-{
-    const string tflipflop = "%";
-    const string tconjunction = "&";
+    const string flipflop = "%";
+    const string conjunction = "&";
 
     const string pattern = @"^((?<name>broadcaster)|((?<type>[%&])(?<name>[a-z]+))) -> (?<dest>\w+)(, (?<dest>\w+))*$";
     var match = Regex.Match(modulestr, pattern, RegexOptions.ExplicitCapture);
@@ -63,15 +70,126 @@ static (string Name, ModuleType Type, IEnumerable<string> Destinations) ParseMod
     {
         throw new ArgumentException($"couldn't parse module string: {modulestr}");
     }
-    return (
-        match.Groups["name"].Value,
-        match.Groups["type"].Value switch { "" => ModuleType.Broadcast, tflipflop => ModuleType.FlipFlop, tconjunction => ModuleType.Conjunction },
-        match.Groups["dest"].Captures.Select(c => c.Value).ToArray()
-    );
+
+    string name = match.Groups["name"].Value;
+    string[] destinations = match.Groups["dest"].Captures.Select(c => c.Value).ToArray();
+
+    return match.Groups["type"].Value switch
+    {
+        "" => new Broadcast(queue, name, destinations),
+        flipflop => new FlipFlop(queue, name, destinations),
+        conjunction => new Conjunction(queue, name, destinations),
+    };
 }
 
-enum Pulse : sbyte { None = 0, Low = -1, High = 1 }
+enum PulseType : sbyte { None = 0, Low = -1, High = 1 }
 
 enum ModuleType { Broadcast, FlipFlop, Conjunction }
 
-enum FlipFlopState { On, Off }
+record Pulse(string? From, string To, PulseType Type)
+{
+    public override string ToString() => $"{From} -{Type}-> {To}";
+}
+
+abstract class Module
+{
+    public readonly string Name;
+    public readonly IEnumerable<string> Destinations;
+
+    private readonly Queue<Pulse> queue;
+
+    public Module(Queue<Pulse> queue, string name, IEnumerable<string> destinations)
+    {
+        this.Name = name;
+        this.Destinations = destinations;
+
+        this.queue = queue;
+    }
+
+    protected void SendPulse(PulseType type)
+    {
+        foreach (var d in Destinations)
+        {
+            var pulse = new Pulse(this.Name, d, type);
+            queue.Enqueue(pulse);
+        }
+    }
+
+    public abstract void HandlePulse(Pulse transmission);
+}
+
+class Broadcast : Module
+{
+    public Broadcast(Queue<Pulse> queue,
+                    string name,
+                    IEnumerable<string> destinations) : base(queue, name, destinations) { }
+
+    // "sends the same pulse to all of its destination modules"
+    public override void HandlePulse(Pulse transmission) => SendPulse(transmission.Type);
+}
+
+class FlipFlop : Module
+{
+    private enum FlipFlopState { On, Off }
+
+    private FlipFlopState state;
+
+    public FlipFlop(Queue<Pulse> queue,
+                    string name,
+                    IEnumerable<string> destinations) : base(queue, name, destinations)
+    {
+        this.state = FlipFlopState.Off; // "they are initially off"
+    }
+
+    public override void HandlePulse(Pulse transmission)
+    {
+        var (type, newstate) = transmission.Type switch
+        {
+            // "If a flip-flop module receives a high pulse, it is ignored and nothing happens."
+            PulseType.High => (PulseType.None, state),
+
+            // "If a flip-flop module receives a low pulse, it flips between on and off."
+            PulseType.Low => state switch
+            {
+                FlipFlopState.On => (PulseType.Low, FlipFlopState.Off),  // "If it was on, it turns off and sends a low pulse."
+                FlipFlopState.Off => (PulseType.High, FlipFlopState.On), // "If it was off, it turns on and sends a high pulse."
+            },
+        };
+
+        this.state = newstate;
+
+        if (type != PulseType.None)
+        {
+            SendPulse(type);
+        }
+    }
+}
+
+class Conjunction : Module
+{
+    private PulseType defaultType = PulseType.Low;
+
+    // "remember the type of the most recent pulse received from each of their
+    //  connected input modules"
+    private readonly IDictionary<string, PulseType> received;
+
+    public Conjunction(Queue<Pulse> queue,
+                       string name,
+                       IEnumerable<string> destinations) : base(queue, name, destinations)
+    {
+        this.received = new Dictionary<string, PulseType>();
+    }
+
+    public override void HandlePulse(Pulse transmission)
+    {
+        // "first updates its memory for that input"
+        received[transmission.From] = transmission.Type;
+
+        // "If it remembers high pulses for all inputs..."
+        var type = received.Values.All(p => p == PulseType.High)
+            ? PulseType.Low   // "it sends a low pulse"
+            : PulseType.High; // "otherwise, it sends a high pulse"
+
+        SendPulse(type);
+    }
+}
