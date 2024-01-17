ce1f0bfc05db0caea861c01645187eb0f6dffad8
George Bills <george.s.bills@gmail.com>
2024-01-17 13:10:11 +0000

    2020d1p2

--- 2020/01/01.hs
+++ 2020/01/01.hs
@@ -1,31 +1,40 @@
-import qualified Data.List as List
-import qualified Data.Set as Set
-
-want :: Int
-want = 2020
+import Data.List qualified as List
+import Data.Set qualified as Set
+import System.Environment as Environment
 
 main = do
-  input <- readInput "in.txt" -- throws?!
-  putStrLn $ show input
-
-  let find2' = find2 want
-  let pair = find2' input
-  putStrLn $ show pair
-
-  let answer = fmap (\(x, y) -> x * y) pair
-  putStrLn $ show answer
-
-readInput :: FilePath -> IO [Int]
-readInput filePath = do
-  contents <- readFile filePath
-  return $ map read $ lines contents
-
-find2 :: Int -> [Int] -> Maybe (Int, Int)
-find2 n ns =
-  case x1 of
-    Just x  -> Just (x, n - x)
-    Nothing -> Nothing
+  args <- Environment.getArgs
+  let f = if null args then "eg.txt" else head args
+  input <- parseInput f
+  print $ part1 sum input
+  print $ part2 sum input
   where
-    set = Set.fromList ns
-    pred = \x -> Set.member (n - x) set
-    x1 = List.find pred ns
+    sum = 2020
+
+parseInput :: FilePath -> IO [Int]
+parseInput fp = do
+  c <- readFile fp
+  return $ map read $ lines c
+
+part1 :: Int -> [Int] -> Maybe Int
+part1 = partX 2
+
+part2 :: Int -> [Int] -> Maybe Int
+part2 = partX 3
+
+partX :: Int -> Int -> [Int] -> Maybe Int
+partX x sum ns = do
+  xs <- findN x sum $ Set.fromList ns
+  return $ product xs
+
+findN :: Int -> Int -> Set.Set Int -> Maybe [Int]
+findN n sum set
+  | n < 1 = error "n must be positive"
+  | n == 1 = if Set.member sum set then Just [sum] else Nothing
+  | n > Set.size set = Nothing
+  | otherwise =
+      let (head, tail) = Set.deleteFindMin set
+          sub = findN (n - 1) (sum - head) tail
+       in case sub of
+            Just sub -> Just (head : sub)
+            Nothing -> findN n sum tail
